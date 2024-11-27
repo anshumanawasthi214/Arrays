d705402bcdfdedba2c009f7fc649198a2257ce31
anshumanawasthi214 <awasthiengineer214@gmail.com>
2024-11-27 23:03:04 +0530

    removed unnecessary element

--- Subarrays.java
+++ Subarrays.java
@@ -1,8 +1,6 @@
 public class Subarrays {
     public static void main(String[] args) {
         int a[]={2,4,6,8,10};
-        int count=0;
-        
         for(int i=0;i<a.length-1;i++){
             for(int j=i+1;j<a.length;j++){
                 System.out.print("( ");
